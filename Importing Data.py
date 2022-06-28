2750c33b286ddcf70f670c5d4bdd18643e7dbdfe
manvitham <makam.manvitha@gmail.com>
2022-06-28 10:19:00 +0100

    added sentiment analysis

--- Importing Data.py
+++ Importing Data.py
@@ -1,8 +1,48 @@
 
-
+#from ipython import display
+import math
+from pprint import pprint
 import pandas as pd
+import numpy as np
+import nltk
+import matplotlib.pyplot as plt
+import seaborn as sns
+from IPython import display
+
+sns.set(style='darkgrid', context='talk', palette='Dark2')
 
 
-df = pd.read_excel(r'C:\Users\makam\Downloads\dec2016statebystate.xlsx', sheet_name="Main Data")
-print(df.head(5))
+if __name__ == '__main__':
+    import praw
+
+    reddit = praw.Reddit(client_id='yRWMyKR8lTnbHRxk1w1WDA',
+                         client_secret='RDJHMQEhwRioRNKg0ZR4B6lNJYd0tQ',
+                         user_agent='PsychologicalRest307',
+                         check_for_async=False)
+
+    subreddit = reddit.subreddit('roevwade2022')
+    news = [*subreddit.top(limit=None)]
+    print(len(news))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #headlines = set()
+
+    #for submission in reddit.subreddit('roe_v_wade').new(limit=None):
+    #headlines.add(submission.title)
+    #display.clear_output()
+    #print(len(headlines))
 
